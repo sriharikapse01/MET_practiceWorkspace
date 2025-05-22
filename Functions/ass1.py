685458d24a18273128e1d5163a20e094766c34b1
kapse srihari <sriharikapse2007@gmail.com>
2025-05-22 18:09:02 +0530

    functions first problem

---
+++ Functions/ass1.py
@@ -0,0 +1,21 @@
+
+def one_to_three():
+    print(1)
+    print(2)
+    print(3)
+    
+def three_to_seven():
+    print(3)
+    print(4)
+    print(5)
+    print(6)
+    print(7)
+    
+def seven_to_ten():
+    print(7)
+    print(8)
+    print(9)
+    print(10)
+    
+three_to_seven()
+seven_to_ten()
