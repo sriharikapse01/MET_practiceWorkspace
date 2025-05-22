4afd4e821f56185da4e791c0093feff7eae18284
kapse srihari <sriharikapse2007@gmail.com>
2025-05-22 18:29:09 +0530

    second project of functions by parameters

---
+++ Functions/ass2.py
@@ -0,0 +1,16 @@
+def greet(lang , name):
+
+    if lang == "en":
+
+        print(f"Hello {name}")
+
+    elif lang == "fr":
+
+        print(f"Bonjour {name}")
+
+    else:
+
+        print(f"Hola {name}")
+
+greet("en", "John")
+
